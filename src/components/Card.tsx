6d171b101a435dd2c4fb532bf72337608f9208cb
Benoit Balcon <benoit.b.balcon@renault.com>
2022-05-18 16:52:10 +0200

    new Card component and styles

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,20 @@
 import React from "react";
 
-export const Card = () => {
-  return <div>Card</div>;
+import "./Card.css";
+
+interface Props {
+  symbol: string;
+  cardStyle: string;
+  index: number;
+  onClick: () => void;
+}
+
+const HIDDEN_SYMBOL: string = "❓";
+
+export const Card: React.FC<Props> = ({ symbol, cardStyle, index, onClick }) => {
+  return (
+    <div className={`card ${cardStyle}`} onClick={() => onClick()}>
+      <span className="symbol">{cardStyle === "hidden" ? HIDDEN_SYMBOL : symbol}</span>
+    </div>
+  );
 };
