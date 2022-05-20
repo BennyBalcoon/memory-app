7912e93197163e4d3aaf43f73d7c1aedfe626529
Benoit Balcon <benoit.b.balcon@renault.com>
2022-05-20 17:58:43 +0200

    improvment to prevent cliking on flipped card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,20 +1,30 @@
 import React from "react";
 
 import "./Card.css";
+import { useGameContext } from "../context/GameContext";
 
 interface Props {
   symbol: string;
   cardStyle: string;
   index: number;
   onClick: () => void;
+  isCardFlipped: boolean;
+  progress: number;
 }
 
 const HIDDEN_SYMBOL: string = "❓";
 
-export const Card: React.FC<Props> = ({ symbol, cardStyle, index, onClick }) => {
+const Card: React.FC<Props> = ({ symbol, cardStyle, index, onClick, isCardFlipped, progress }) => {
+  const { isGameOn } = useGameContext();
+
+  const handleClick = () => {
+    !isCardFlipped && isGameOn && progress < 100 && onClick();
+  };
   return (
-    <div className={`card ${cardStyle}`} onClick={() => onClick()}>
+    <div className={`card ${cardStyle}`} onClick={handleClick}>
       <span className="symbol">{cardStyle === "hidden" ? HIDDEN_SYMBOL : symbol}</span>
     </div>
   );
 };
+
+export default Card;
