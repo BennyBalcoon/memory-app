e2f86218663b00a92d4ca5bc3832f37cf26d6710
Benoit Balcon <benoit.b.balcon@renault.com>
2022-05-20 18:00:05 +0200

    new input component to enter winner's name

---
+++ src/components/WinnerInput.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+const WIN_KEY = "memory-winner";
+const WIN_MAX = 3;
+
+interface Props {
+  guesses: number;
+  updateWinnersList: (arg0: []) => void;
+}
+
+const WinnerInput = ({ guesses, updateWinnersList }: Props) => {
+  const [winner, setWinner] = useState<string>("");
+
+  const handleWinnerSubmit: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    setWinner(event.target.value);
+  };
+
+  const storeWinner = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const newWinner = {
+      guesses: guesses,
+      winner: winner,
+      date: new Date().toLocaleDateString(),
+      id: Date.now(),
+    };
+
+    const entries = JSON.parse(localStorage.getItem(WIN_KEY) || "[]");
+
+    entries.push(newWinner);
+
+    entries.sort((a: any, b: any) => {
+      return a.guesses - b.guesses;
+    });
+
+    if (entries.length > WIN_MAX) {
+      entries.splice(WIN_MAX, entries.length);
+    }
+
+    localStorage.setItem(WIN_KEY, JSON.stringify(entries));
+    console.log(entries);
+    updateWinnersList(entries);
+  };
+
+  return (
+    <form className="highScoreInput" onSubmit={storeWinner}>
+      <p>
+        <label>
+          Bravo ! Entre ton prénom :
+          <input type="text" autoComplete="given-name" onChange={handleWinnerSubmit} value={winner} />
+        </label>
+        <button type="submit">J’ai gagné !</button>
+      </p>
+    </form>
+  );
+};
+
+export default WinnerInput;
