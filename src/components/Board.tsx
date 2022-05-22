c08ed134ec2c8e6cf73b73214423cd53d507854a
Benoit Balcon <benoit.b.balcon@renault.com>
2022-05-22 13:00:54 +0200

    transferring ProgressBar logic to Board comp

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,24 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import { getShuffledSymbols } from "../symbols";
+import { useGameContext } from "../context/GameContext";
 
 import Card from "./Card";
 import ProgressBar from "./ProgressBar";
 import GuessCounter from "./GuessCounter";
 import WinnerInput from "./WinnerInput";
 import WinnersList from "./WinnersList";
+import Button from "./Button";
 
 import "./Board.css";
 
 const WIN_KEY = "memory-winner";
 
 const Board = () => {
-  const [symbols, setSymbols] = useState(getShuffledSymbols());
+  const [symbols, setSymbols] = useState<Array<string>>(getShuffledSymbols());
   const [currentPair, setCurrentPair] = useState<Array<number>>([]);
   const [matchedCardsIndex, setMatchedCardsIndex] = useState<Array<number>>([]);
   const [progress, setProgress] = useState<number>(0);
   const [guesses, setGuesses] = useState<number>(0);
   const [entries, setEntries] = useState(null);
+  const { isGameOn, toggleGame } = useGameContext();
 
   const updateWinnersList = () => {
     setEntries(JSON.parse(localStorage.getItem(WIN_KEY) || "[]"));
@@ -50,8 +53,8 @@ const Board = () => {
     }
 
     const newPair: Array<number> = [currentPair[0], index];
-    const newGuesses = guesses + 1;
-    const matched = symbols[newPair[0]] === symbols[newPair[1]];
+    const newGuesses: number = guesses + 1;
+    const matched: boolean = symbols[newPair[0]] === symbols[newPair[1]];
     setCurrentPair(newPair);
     setGuesses(newGuesses);
     matched && setMatchedCardsIndex([...matchedCardsIndex, ...newPair]);
@@ -65,6 +68,14 @@ const Board = () => {
     return currentPair.includes(index);
   };
 
+  const handleStart = () => {
+    toggleGame?.();
+  };
+
+  const handleStop = () => {
+    toggleGame?.();
+  };
+
   const handleRestart = () => {
     setGuesses(0);
     setProgress(0);
@@ -74,7 +85,29 @@ const Board = () => {
   };
 
   const won: boolean = matchedCardsIndex.length === symbols.length && progress <= 100;
+
   const lose: boolean = progress >= 100 && matchedCardsIndex.length !== symbols.length;
+
+  useEffect(() => {
+    let timer: ReturnType<typeof setInterval>;
+    if (isGameOn) {
+      timer = setInterval(() => {
+        setProgress((progress) => progress + 1);
+      }, 1000);
+    }
+    return () => {
+      if (progress === 100) {
+        console.log("ici");
+        clearInterval(timer);
+        toggleGame?.();
+      }
+      if (won) {
+        clearInterval(timer);
+        toggleGame?.();
+      }
+      clearInterval(timer);
+    };
+  }, [progress, isGameOn, toggleGame, won]);
 
   return (
     <>
@@ -91,16 +124,17 @@ const Board = () => {
             progress={progress}
           />
         ))}
-        {won &&
-          (entries ? (
-            <WinnersList winners={entries} />
-          ) : (
-            <WinnerInput guesses={guesses} updateWinnersList={updateWinnersList} />
-          ))}
+        {entries && <WinnersList winners={entries} />}
+        {won && <WinnerInput guesses={guesses} updateWinnersList={updateWinnersList} />}
         {lose && <p>Tu as perdu ! Clique sur Reset et retente ta chance</p>}
       </div>
       <div>
-        <ProgressBar bgcolor="blue" progress={progress} setProgress={setProgress} handleRestart={handleRestart} />
+        <ProgressBar progress={progress} />
+      </div>
+      <div className="btn_container">
+        {!isGameOn && <Button label={"Start"} onClick={handleStart} />}
+        {isGameOn && progress < 100 && <Button label={"Stop"} onClick={handleStop} />}
+        {(!isGameOn || progress >= 100) && <Button label={"Reset"} onClick={handleRestart} />}
       </div>
     </>
   );
--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,67 +1,23 @@
-import React, { useEffect, useState } from "react";
-import Button from "./Button";
+import React from "react";
 
 import "./ProgressBar.css";
-import { useGameContext } from "../context/GameContext";
-
 interface Props {
-  bgcolor: string;
   progress: number;
-  setProgress: React.Dispatch<React.SetStateAction<number>>;
-  handleRestart: () => void;
 }
 
-const ProgressBar = ({ bgcolor, progress, setProgress, handleRestart }: Props) => {
-  const [isActive, setIsActive] = useState<boolean>(false);
-  const { isGameOn, toggleGame } = useGameContext();
-  console.log("first render", isGameOn);
-
-  const handleStart = () => {
-    toggleGame?.();
-    setIsActive(true);
-  };
-
-  const handleStop = () => {
-    toggleGame?.();
-    setIsActive(false);
-  };
-
-  useEffect(() => {
-    let timer: ReturnType<typeof setInterval>;
-    if (isActive) {
-      timer = setInterval(() => {
-        setProgress((progress) => progress + 1);
-      }, 1000);
-    }
-    return () => {
-      if (progress >= 99) {
-        clearInterval(timer);
-        setIsActive(false);
-      }
-      clearInterval(timer);
-    };
-  });
-
+const ProgressBar = ({ progress }: Props) => {
   const fillerStyles = {
     height: "100%",
     width: `${progress}%`,
-    backgroundColor: bgcolor,
+    backgroundColor: "#2f74c0",
     borderRadius: "inherit",
-    transition: "width 1s ease-in-out",
+    transition: "width 1s linear",
   };
-
-  console.log(progress);
 
   return (
     <div className="pb_container">
       <div style={fillerStyles}>
         <span className="pb_content"></span>
-      </div>
-      <div className="btn_container">
-        {!isGameOn && <Button label={"Start"} onClick={handleStart} />}
-
-        {progress < 100 && <Button label={"Stop"} onClick={handleStop} />}
-        {(!isGameOn || progress >= 100) && <Button label={"Reset"} onClick={handleRestart} />}
       </div>
     </div>
   );
